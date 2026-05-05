87ead5ecdde3f9a6d4c3e0e65677608d75e703c1
Praneeth Reddy <dyapapraneethreddy1@gmail.com>
2026-05-05 15:00:05 +0530

    updated stock model

--- backend/models/StockModel.js
+++ backend/models/StockModel.js
@@ -1,4 +1,4 @@
-import {schema , model} from "mongoose";
+import {Schema , model} from "mongoose";
 
 //stock model   
 
@@ -6,7 +6,7 @@ import {schema , model} from "mongoose";
 // currentPrice, availableQuantity, latestTradingDate, previousClosePrice, 
 // changeInPrice, changePercentage
 
-const stockSchema = new schema({
+const stockSchema = new Schema({
     stockSymbol:{
         type:String,
         required:[true,"stock Symbol is required"],
@@ -16,45 +16,17 @@ const stockSchema = new schema({
         type:String,
         required:[true,"company Name is required"]
     },
-    PriceWhenMarketOpen:{
-        type:Number,
-        required:[true,"Price When Market Open is required"]
-    },
-    highestPrice:{
-        type:Number,
-        required:[true,"highest Price is required"]  
-    },
-    lowestPrice:{
-        type:Number,
-        required:[true,"lowest Price is required"]
-    },
-    currentPrice:{
-        type:Number,
-        required:[true,"current Price is required"]
+    sector:{
+        type:String,
+        required:[true,"Sector is required"]
     },
     availableQuantity:{
         type:Number,
         required:[true,"available Quantity is required"]
-    },
-    latestTradingDate:{
-        type:Date,
-        required:[true,"latest Trading Date is required"]
-    },
-    previousClosePrice:{
-        type:Number,
-        required:[true,"previous Close Price is required"]
-    },
-    changeInPrice:{
-        type:Number,
-        required:[true,"change In Price is required"]
-    },
-    changePercentage:{
-        type:Number,
-        required:[true,"change Percentage is required"]
     }},{
     timestamps:true,
-     versionKey: false,
-    strict:"throw"
-});
+    versionKey:false
+    }
+);
 
 export const stockModel = model("stock",stockSchema)
