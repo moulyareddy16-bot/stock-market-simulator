1bc7c2715b7b520f20033655b92cb950f4691008
Moulya Reddy <moulyareddy16@gmail.com>
2026-05-05 10:58:33 +0530

    updated server and app

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,59 @@
 import exp from "express"
+import cookieParser from "cookie-parser";
+import cors from 'cors'
 
-import {authRouter} from "./routes/authRoute"
+import authRouter from "./routes/authRoute"
 
-const app = express()
+//create express app
+const app = exp();
 
-// Middleware
+//enable cors
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}))
+
+//add cookie parser middeleware
+app.use(cookieParser())
+
+//body parser middleware
 app.use(exp.json());
 
 // Routes
 app.use("/api/auth", authRouter);
+
+//to handle invalid path
+app.use((req, res, next) => {
+  console.log(req.url);
+  res.status(404).json({ message: `path ${req.url} is invalid` });
+});
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+  console.log("error is ",err)
+  console.log("Full error:", JSON.stringify(err, null, 2));
+  //ValidationError
+  if (err.name === "ValidationError") {
+    return res.status(400).json({ message: "error occurred", error: err.message });
+  }
+  //CastError
+  if (err.name === "CastError") {
+    return res.status(400).json({ message: "error occurred", error: err.message });
+  }
+  const errCode = err.code ?? err.cause?.code ?? err.errorResponse?.code;
+  const keyValue = err.keyValue ?? err.cause?.keyValue ?? err.errorResponse?.keyValue;
+
+  if (errCode === 11000) {
+    const field = Object.keys(keyValue)[0];
+    const value = keyValue[field];
+    return res.status(409).json({
+      message: "error occurred",
+      error: `${field} "${value}" already exists`,
+    });
+  }
+
+  //send server side error
+  res.status(500).json({ message: "error occurred", error: "Server side error" });
+});
 
 export default app;
