f5a1df7f484a63e3d0b222423fb69ec77a2f1c9f
Praneeth Reddy <dyapapraneethreddy1@gmail.com>
2026-05-05 14:30:41 +0530

    abc

--- backend/models/StockModel.js
+++ backend/models/StockModel.js
@@ -1,4 +1,4 @@
-import {Schema , model} from "mongoose";
+import {schema , model} from "mongoose";
 
 //stock model   
 
@@ -6,7 +6,7 @@ import {Schema , model} from "mongoose";
 // currentPrice, availableQuantity, latestTradingDate, previousClosePrice, 
 // changeInPrice, changePercentage
 
-const stockSchema = new Schema({
+const stockSchema = new schema({
     stockSymbol:{
         type:String,
         required:[true,"stock Symbol is required"],
@@ -16,23 +16,45 @@ const stockSchema = new Schema({
         type:String,
         required:[true,"company Name is required"]
     },
-    sector:{
-        type:String,
-        required:[true,"Sector is required"]
+    PriceWhenMarketOpen:{
+        type:Number,
+        required:[true,"Price When Market Open is required"]
+    },
+    highestPrice:{
+        type:Number,
+        required:[true,"highest Price is required"]  
+    },
+    lowestPrice:{
+        type:Number,
+        required:[true,"lowest Price is required"]
+    },
+    currentPrice:{
+        type:Number,
+        required:[true,"current Price is required"]
     },
     availableQuantity:{
         type:Number,
         required:[true,"available Quantity is required"]
+    },
+    latestTradingDate:{
+        type:Date,
+        required:[true,"latest Trading Date is required"]
+    },
+    previousClosePrice:{
+        type:Number,
+        required:[true,"previous Close Price is required"]
+    },
+    changeInPrice:{
+        type:Number,
+        required:[true,"change In Price is required"]
+    },
+    changePercentage:{
+        type:Number,
+        required:[true,"change Percentage is required"]
     }},{
     timestamps:true,
-<<<<<<< HEAD
-    versionKey:false
-    }
-);
-=======
      versionKey: false,
     strict:"throw"
 });
->>>>>>> 339d18e5855ced6c2881f64b8d6206ac91166514
 
 export const stockModel = model("stock",stockSchema)
