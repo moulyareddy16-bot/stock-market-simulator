dff5fcf74e4471c6ef0976fa2355162b937ba9e4
Moulya Reddy <moulyareddy16@gmail.com>
2026-05-06 10:27:42 +0530

    modified transactions

--- backend/routes/transactionRoute.js
+++ backend/routes/transactionRoute.js
@@ -14,14 +14,10 @@ const transactionRouter = exp.Router()
 transactionRouter.post("/buy",verifyToken("trader"), buyStock);
 
 // Sell stock
-<<<<<<< HEAD
-transactionRouter.post("/sell",verifyToken("trader", sellStock));
-=======
 transactionRouter.post("/sell",verifyToken("trader"), sellStock);
->>>>>>> 2dc15d36f1937eac903d6dcab2e62efa0ee45614
 
 // Get transaction history
-transactionRouter.get("/",verifyToken("trader"), getTransactions);
+transactionRouter.get("/history",verifyToken("trader"), getTransactions);
 
 
 export default transactionRouter;
